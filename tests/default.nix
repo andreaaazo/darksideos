02d3a0c111de96b7bd543790ddcfb1f385fdc9a2
andreaaazo <zorzi.andrea@outlook.com>
2026-04-05 21:09:43 +0300

    fix(evaltest): removed lib import to fix error unexpected argument

--- tests/default.nix
+++ tests/default.nix
@@ -3,7 +3,7 @@
 # Structure mirrors shared-modules/ for discoverability.
 #
 # Usage in flake.nix:
-#   checks.x86_64-linux = import ./tests { inherit pkgs lib nixpkgs; };
+#   evalTests.x86_64-linux = import ./tests { inherit pkgs lib nixpkgs; };
 {
   pkgs,
   lib,
@@ -12,9 +12,9 @@
   testLib = import ./lib {inherit pkgs lib nixpkgs;};
 in {
   # core/
-  eval-core-boot = import ./eval/core/boot.nix {inherit pkgs lib testLib;};
-  eval-core-locale = import ./eval/core/locale.nix {inherit pkgs lib testLib;};
-  eval-core-networking = import ./eval/core/networking.nix {inherit pkgs lib testLib;};
-  eval-core-nix = import ./eval/core/nix.nix {inherit pkgs lib testLib;};
-  eval-core-users = import ./eval/core/users.nix {inherit pkgs lib testLib;};
+  eval-core-boot = import ./eval/core/boot.nix {inherit pkgs testLib;};
+  eval-core-locale = import ./eval/core/locale.nix {inherit pkgs testLib;};
+  eval-core-networking = import ./eval/core/networking.nix {inherit pkgs testLib;};
+  eval-core-nix = import ./eval/core/nix.nix {inherit pkgs testLib;};
+  eval-core-users = import ./eval/core/users.nix {inherit pkgs testLib;};
 }
