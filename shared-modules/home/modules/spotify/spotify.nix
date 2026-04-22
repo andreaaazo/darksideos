f86e6a9df71a555ebf3270fff339ed2be2a7ceab
Andrea <zorzi.andrea@outlook.com>
2026-04-23 00:51:46 +0200

    feat(vader-host): added spotify login persistence, kitty config

--- shared-modules/home/modules/spotify/spotify.nix
+++ shared-modules/home/modules/spotify/spotify.nix
@@ -2,6 +2,17 @@
   # Install Spotify desktop client in the user profile.
   home-manager.users.andrea.home.packages = [pkgs.spotify];
 
+home-manager.users.andrea.home.persistence."/persist" = {directories = [
+".config/spotify"
+".cache/spotify/Default"
+".cache/spotify/Users"
+".cache/spotify/Storage"
+];
+files = [
+".cache/spotify/Local State"
+
+];
+};
   # Hyprland keybinding list to launch media applications.
   home-manager.users.andrea.wayland.windowManager.hyprland.settings.bind = [
     # SPOTIFY: Launch Spotify natively on Wayland
